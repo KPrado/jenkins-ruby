87b1492f5cb5556423bb131789acb09d11aa5fd5
KPrado <kaiquegrdp@gmail.com>
2022-05-01 19:13:32 -0300

    fixing enter key

--- features/step_definitions/google_steps.rb
+++ features/step_definitions/google_steps.rb
@@ -6,7 +6,7 @@ end
 
 Quando("pesquisar por {string}") do |texto_pesquisa|
     find("input[name=q]").set texto_pesquisa
-    find("input[name=q]")send_keys :enter
+    find("input[name=q]").send_keys :enter
 end
 
 Então("deverá ser exibida pesquisa relacionada") do
